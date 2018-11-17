af6d6e47c51a4a3c5de2ad523b536ff18b11da1b
lionelStuart <lisi_stuart@126.com>
2018-11-17 20:23:10 +0800

    add email add blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post, Comment
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publish', 'status')
+    # 过滤返回结果
+    list_filter = ('status', 'created', 'publish', 'author')
+    # 定义搜索字段
+    search_fields = ('title', 'body')
+    # 自动填充标题
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    # 快速导航
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, re_path
+
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+    # url(r'^$', views.PostListView.as_view(), name='post_list'),
+    re_path('^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$',
+            views.post_detail,
+            name='post_detail'),
+    # url(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
+]
